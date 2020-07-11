3bd8e3c4051020be142777a9f68aaa8f9b079b8f
sidseal <sidseal@me.com>
2020-07-11 18:52:03 -0400

    fixes

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || {
+mongoose.connect(process.env.MONGODB_URI || , {
   useNewUrlParser: true,
 });
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlroutes.js"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
